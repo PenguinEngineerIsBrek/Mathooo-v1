0db7d82fbdd6490fb0b9387f2d052f8f321cc6da
PenguinEngineerIsBrek <105980454+PenguinEngineerIsBrek@users.noreply.github.com>
2023-07-08 08:15:53 +0300

    Fixed the parser and added the interpreter

--- src/shell.py
+++ src/shell.py
@@ -1,8 +1,12 @@
-import tokenhandler
+import tokenhandler, os
+
+os.path.basename(__file__)
+
 
 while True:
-    text = input('MATH > ')
-    result, error = tokenhandler.run(text)
+		text = input('MATH > ')
+		result, error = tokenhandler.run(f'{__file__}', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+		if error: print(error.as_string())
+		else: print(result)
+		
